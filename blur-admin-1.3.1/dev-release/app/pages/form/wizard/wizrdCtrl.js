67c0480723be97290af5abca1af41860ee305c97
tnguyen449 <tnguyen.5593@gmail.com>
2017-07-08 17:18:09 +0700

    update wizard.html, tables.html, tables.module.js, TablesPageCtrl.js,wizrdCtrl.js, smartTable.html
    Signed-off-by: tnguyen449 <tnguyen.5593@gmail.com>

--- blur-admin-1.3.1/dev-release/app/pages/form/wizard/wizrdCtrl.js
+++ blur-admin-1.3.1/dev-release/app/pages/form/wizard/wizrdCtrl.js
@@ -1,21 +1,32 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular.module('BlurAdmin.pages.form')
-      .controller('WizardCtrl', WizardCtrl);
+    angular.module('BlurAdmin.pages.form')
+        .controller('WizardCtrl', WizardCtrl);
 
-  /** @ngInject */
-  function WizardCtrl($scope) {
-   var vm = this;
+    /** @ngInject */
+    function WizardCtrl($scope) {
+        var vm = this;
 
-    vm.personalInfo = {};
-    vm.productInfo = {};
-    vm.shipment = {};
+        vm.personalInfo = {};
+        vm.productInfo = {};
+        vm.shipment = {};
 
-    vm.arePersonalInfoPasswordsEqual = function () {
-      return vm.personalInfo.confirmPassword && vm.personalInfo.password == vm.personalInfo.confirmPassword;
-    };
-  }
+        vm.arePersonalInfoPasswordsEqual = function() {
+            return vm.personalInfo.confirmPassword && vm.personalInfo.password == vm.personalInfo.confirmPassword;
+        };
+        vm.Id = function() {
 
+            var date = new Date();
+
+            var d = (date.getDate()).toString();
+            var m = (date.getMonth() + 1).toString();
+            var y = date.getFullYear().toString();
+            var h = date.getHours().toString();
+            var min = date.getMinutes().toString();
+            var sec = date.getSeconds().toString();
+            return "SQ1NT" + d + m + y + h + min + sec;
+        }
+
+    }
 })();
-
